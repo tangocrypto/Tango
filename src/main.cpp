5501792f051857173cd0cf79a328ddfdb4edce10
KingricharDV#0439 <northerncommunity1@gmail.com>
2019-06-05 22:29:44 -0400

    update
    update

--- src/main.cpp
+++ src/main.cpp
@@ -58,10 +58,12 @@ using namespace std;
  */
 
 CCriticalSection cs_main;
+CCriticalSection cs_mapstake;
 
 BlockMap mapBlockIndex;
 map<uint256, uint256> mapProofOfStake;
 set<pair<COutPoint, unsigned int> > setStakeSeen;
+map<COutPoint, int> mapStakeSpent;
 map<unsigned int, unsigned int> mapHashedBlocks;
 CChain chainActive;
 CBlockIndex* pindexBestHeader = NULL;
@@ -1107,7 +1109,7 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState& state, const CTransa
 
     //Coinstake is also only valid in a block, not as a loose transaction
     if (tx.IsCoinStake())
-        return state.DoS(100, error("AcceptToMemoryPool: coinstake as individual tx"),
+        return state.DoS(100, error("AcceptToMemoryPool: coinstake as individual tx. txid=%s", tx.GetHash().GetHex()),
             REJECT_INVALID, "coinstake");
 
     // Rather not work on nonstandard transactions (unless -testnet/-regtest)
@@ -1835,7 +1837,7 @@ bool CScriptCheck::operator()()
 
 bool CheckInputs(const CTransaction& tx, CValidationState& state, const CCoinsViewCache& inputs, bool fScriptChecks, unsigned int flags, bool cacheStore, std::vector<CScriptCheck>* pvChecks)
 {
-    
+
 	CBlockIndex* pindexPrev = mapBlockIndex.find(inputs.GetBestBlock())->second;
 	if (!tx.IsCoinBase() && pindexPrev->nHeight) {
         if (pvChecks)
@@ -1878,7 +1880,7 @@ bool CheckInputs(const CTransaction& tx, CValidationState& state, const CCoinsVi
                                           tx.GetHash().ToString(), FormatMoney(nValueIn), FormatMoney(tx.GetValueOut())),
                      REJECT_INVALID, "bad-txns-in-belowout");
 
-             
+
             CAmount nTxFee = nValueIn - tx.GetValueOut();
             if (nTxFee < 0 && pindexPrev->nHeight)
                 return state.DoS(100, error("CheckInputs() : %s nTxFee < 0", tx.GetHash().ToString()),
@@ -2009,6 +2011,10 @@ bool DisconnectBlock(CBlock& block, CValidationState& state, CBlockIndex* pindex
                 if (coins->vout.size() < out.n + 1)
                     coins->vout.resize(out.n + 1);
                 coins->vout[out.n] = undo.txout;
+
+                LOCK(cs_mapstake);
+                    // erase the spent input
+                    mapStakeSpent.erase(out);
             }
         }
     }
@@ -2240,6 +2246,31 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     if (fTxIndex)
         if (!pblocktree->WriteTxIndex(vPos))
             return state.Abort("Failed to write transaction index");
+
+
+{
+        LOCK(cs_mapstake);
+
+        // add new entries
+        for (const CTransaction tx: block.vtx) {
+            if (tx.IsCoinBase())
+                continue;
+            for (const CTxIn in: tx.vin) {
+                mapStakeSpent.insert(std::make_pair(in.prevout, pindex->nHeight));
+            }
+    }
+
+  // delete old entries
+        for (auto it = mapStakeSpent.begin(); it != mapStakeSpent.end();) {
+            if (it->second < pindex->nHeight - Params().MaxReorganizationDepth()) {
+                it = mapStakeSpent.erase(it);
+            } else {
+                it++;
+            }
+        }
+    }
+
+
 
     // add this block to the view's block chain
     view.SetBestBlock(pindex->GetBlockHash());
@@ -3403,6 +3434,64 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
 
     int nHeight = pindex->nHeight;
 
+    if (block.IsProofOfStake()) {
+       LOCK(cs_main);
+
+       CCoinsViewCache coins(pcoinsTip);
+
+       if (!coins.HaveInputs(block.vtx[1])) {
+
+ LOCK(cs_mapstake);
+
+           // the inputs are spent at the chain tip so we should look at the recently spent outputs
+           for (CTxIn in : block.vtx[1].vin) {
+               auto it = mapStakeSpent.find(in.prevout);
+               if (it == mapStakeSpent.end()) {
+                   return false;
+               }
+               if (it->second < pindexPrev->nHeight) {
+                   return false;
+               }
+           }
+       }
+
+       // if this is on a fork
+       if (!chainActive.Contains(pindexPrev) && pindexPrev != NULL) {
+
+           // start at the block we're adding on to
+           CBlockIndex *last = pindexPrev;
+
+           // while that block is not on the main chain
+           while (!chainActive.Contains(last) && pindexPrev != NULL) {
+               CBlock bl;
+               ReadBlockFromDisk(bl, last);
+
+               // loop through every spent input from said block
+               for (CTransaction t : bl.vtx) {
+                   for (CTxIn in: t.vin) {
+
+                       // loop through every spent input in the staking transaction of the new block
+                       for (CTxIn stakeIn : block.vtx[1].vin) {
+
+                           // if they spend the same input
+                           if (stakeIn.prevout == in.prevout) {
+
+                               // reject the block
+                               return false;
+                           }
+                       }
+                   }
+               }
+
+               // go to the parent block
+               last = last->pprev;
+           }
+       }
+   }
+
+
+
+
     // Write block to history file
     try {
         unsigned int nBlockSize = ::GetSerializeSize(block, SER_DISK, CLIENT_VERSION);
@@ -3548,7 +3637,7 @@ bool ProcessNewBlock(CValidationState& state, CNode* pfrom, CBlock* pblock, CDis
             pwalletMain->AutoCombineDust();
     }
 
-    LogPrintf("%s : ACCEPTED in %ld milliseconds with size=%d\n", __func__, GetTimeMillis() - nStartTime,
+    LogPrintf("%s : ACCEPTED Block %ld in %ld milliseconds with size=%d\n", __func__, GetHeight(), GetTimeMillis() - nStartTime,
               pblock->GetSerializeSize(SER_DISK, CLIENT_VERSION));
 
     return true;
@@ -4568,7 +4657,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 {
     RandAddSeedPerfmon();
     if (fDebug)
-        LogPrintf("received: %s (%u bytes) peer=%d\n", SanitizeString(strCommand), vRecv.size(), pfrom->id);
+        LogPrintf(LogPrint("net", "received: %s (%u bytes) peer=%d\n", SanitizeString(strCommand), vRecv.size(), pfrom->id);
     if (mapArgs.count("-dropmessagestest") && GetRand(atoi(mapArgs["-dropmessagestest"])) == 0) {
         LogPrintf("dropmessagestest DROPPING RECV MESSAGE\n");
         return true;
@@ -4578,6 +4667,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
         // Each connection can only send one version message
         if (pfrom->nVersion != 0) {
             pfrom->PushMessage("reject", strCommand, REJECT_DUPLICATE, string("Duplicate version message"));
+            LOCK(cs_main);
             Misbehaving(pfrom->GetId(), 1);
             return false;
         }
