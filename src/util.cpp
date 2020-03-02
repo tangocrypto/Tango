d4f6772863a18e67c50db1b228a9c85124389414
KingricharDV#0439 <northerncommunity1@gmail.com>
2020-03-01 16:59:58 -1000

    update

--- src/util.cpp
+++ src/util.cpp
@@ -121,9 +121,9 @@ string strBudgetMode = "";
 
 map<string, string> mapArgs;
 map<string, vector<string> > mapMultiArgs;
-bool fDebug = false;
+bool fDebug = true;
 bool fPrintToConsole = false;
-bool fPrintToDebugLog = true;
+bool fPrintToDebugLog = false;
 bool fDaemon = false;
 bool fServer = false;
 string strMiscWarning;
