69dab407c3613eeb940f5d8eba5fb5a2f2f306be
KingricharDV#0439 <northerncommunity1@gmail.com>
2020-03-07 11:13:11 -1000

    Update version.h

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70940;
+static const int PROTOCOL_VERSION = 70950;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
